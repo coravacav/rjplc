fbffaee6a5a02fa8a4ed5cf8d3d80eab7ecadae9
Stefan Todorov <stefan.b.todorov@gmail.com>
2025-02-02 23:34:03 -0600

    add a slightly better status for client

--- rjplc-wasm/src/lib.rs
+++ rjplc-wasm/src/lib.rs
@@ -9,8 +9,11 @@ use wasm_bindgen::prelude::*;
 #[derive(Debug, Clone)]
 pub struct Output {
     lex_output: Option<String>,
+    pub lex_success: bool,
     parse_output: Option<String>,
+    pub parse_success: bool,
     type_check_output: Option<String>,
+    pub type_check_success: bool,
 }
 
 #[wasm_bindgen]
@@ -37,8 +40,11 @@ pub fn perform_steps(contents: &str) -> Output {
 
     let mut successful_steps = Output {
         lex_output: None,
+        lex_success: false,
         parse_output: None,
+        parse_success: false,
         type_check_output: None,
+        type_check_success: false,
     };
 
     let (tokens, input_by_token) = match lex::LexLinear::lex(contents) {
@@ -49,6 +55,8 @@ pub fn perform_steps(contents: &str) -> Output {
             return successful_steps;
         }
     };
+
+    successful_steps.lex_success = true;
 
     use std::fmt::Write;
     let mut output = String::new();
@@ -66,6 +74,8 @@ pub fn perform_steps(contents: &str) -> Output {
             return successful_steps;
         }
     };
+
+    successful_steps.parse_success = true;
 
     let mut output = String::new();
 
